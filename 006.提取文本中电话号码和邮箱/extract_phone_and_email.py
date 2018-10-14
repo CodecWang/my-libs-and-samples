f7b6431d3b793b6e72fb72e5d65072a4560b8923
ex2tron <ex2tron@foxmail.com>
2018-10-14 20:18:34 +0800

    每天一工具 2018年10月14日

---
+++ 006.提取文本中电话号码和邮箱/extract_phone_and_email.py
@@ -0,0 +1,26 @@
+# ex2tron's blog:
+# http://ex2tron.wang
+# 任务描述：
+# 搜索出文本中所有的电话号码和邮箱地址
+
+
+import re
+import pyperclip
+
+# 中国手机号码正则匹配
+phone_regex = re.compile(r'((\d{3})(\s|-|\.)?(\d{4})(\s|-|\.)?(\d{4}))')
+# 邮箱地址正则匹配
+email_regex = re.compile(
+    r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+(\.[a-zA-Z]{2,4}))')
+
+text = pyperclip.paste()
+print('匹配到的所有电话号码为：')
+for group in phone_regex.findall(text):
+    print(group[0])
+
+print('匹配到的所有邮箱地址为：')
+for group in email_regex.findall(text):
+    print(group[0])
+
+# 测试文本
+# 我叫ex2tron，电话号码是188-8888-6666，邮箱是：ex2tron@outlook.com，另一个邮箱是：ex2tron@foxmail.com
