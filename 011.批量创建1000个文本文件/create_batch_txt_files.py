2ed8921af6ec7d1e80a597ac5cc9d3456e04bb7e
ex2tron <ex2tron@foxmail.com>
2018-12-05 20:08:11 +0800

    每天一工具 2018年12月5日

---
+++ 011.批量创建1000个文本文件/create_batch_txt_files.py
@@ -0,0 +1,17 @@
+# ex2tron's blog:
+# http://ex2tron.wang
+# 任务描述：
+# 在当前目录下创建dir1并生成000.txt~999.txt 1000个文件
+# 每个文件的内容是文件名本身
+
+import os
+
+# 创建文件夹
+if not os.path.exists('dir1/'):
+    os.mkdir('dir1/')
+
+# 批量生成
+for i in range(100):
+    with open('dir1/%03d.txt' % i, 'w') as f:
+        f.write('%03d' % i)
+        # 内容为空的话，这里直接写pass
