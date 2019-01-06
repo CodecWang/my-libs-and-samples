fb270f94f4d7a18cb2af270b6731f637b3350c9e
ex2tron <ex2tron@foxmail.com>
2019-01-06 21:28:20 +0800

    每天一工具 2019年1月6日

---
+++ 014.Windows快速复制为C格式路径/copy_as_c_format_path.py
@@ -0,0 +1,12 @@
+# ex2tron's blog:
+# http://ex2tron.wang
+# 任务描述：Windows下的默认路径是反斜杠，比如：D:\Surface\Documents\01.Build\MyLibsAndSamples
+# C或者一些语言中这个路径显然是不对的，可以先拷贝这个路径，然后执行脚本，快速转换成双反斜杠或斜杠
+# 2019年1月6日 21点23分
+
+import pyperclip
+
+# 双反斜杠
+cFormat = pyperclip.paste().replace('\\', '\\\\')
+
+pyperclip.copy(cFormat)
