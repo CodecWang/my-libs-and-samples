39d7b8797ef1bc853acf8de1828c60767d1d54c6
ex2tron.ubuntu <ex2tron@outlook.com>
2019-01-08 20:18:28 +0800

    每天一工具 2019年1月8日

---
+++ 015.MySQL数据库快速导出/mysql_fast_dump.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ex2tron's blog:
+# http://ex2tron.wang 
+# 任务描述：快速导出指定数据库的数据，以日期结尾命名
+# 也可以在-p后面直接写上密码，不过不安全，不推荐。
+# 2019年1月8日 20:16
+
+if [ -n "$1" ]; then
+    db_name=$1
+    today=$(date +%Y%m%d)
+    echo "Exporting database: "$db_name
+    mysqldump -uroot -p $db_name > $db_name"_data_"$today.sql
+else
+    echo "Please offer your database name."
+fi
---
+++ 015.MySQL数据库快速导出/mysql_fast_dump_fixed_dbname.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ex2tron's blog:
+# http://ex2tron.wang 
+# 任务描述：快速导出固定数据库的数据，以日期结尾命名
+# 也可以在-p后面直接写上密码，不过不安全，不推荐。
+# 2019年1月8日 20:16
+
+today=$(date +%Y%m%d)
+db_name="your_database_name"
+
+echo "Exporting database: "$db_name
+mysqldump -uroot -p $db_name > $db_name"_data_"$today.sql
